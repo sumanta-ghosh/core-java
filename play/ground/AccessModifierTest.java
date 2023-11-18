a4b110ba0d3cb12fa722fdc8d3cbd018b318816f
Sumanta <sumanta@ncoderslab.in>
2023-11-18 18:56:52 +0530

    Change in mvn project

---
+++ play/ground/AccessModifierTest.java
@@ -0,0 +1,10 @@
+package play.ground;
+
+public class AccessModifierTest {
+    public static void main(String[] args) {
+        // OuterClass myOuter = new OuterClass();
+        // System.out.println(myOuter.name);
+        OuterClass.InnerClass myInner = new OuterClass().new InnerClass();
+        myInner.print();
+    }
+}
